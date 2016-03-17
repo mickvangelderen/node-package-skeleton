0841303500191cd4256b4031e2179f252403ddcc
Mick van Gelderen <mickvangelderen@gmail.com>
2016-03-17 23:01:53 +0100

    Add modular css support

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,6 @@
-console.warn('Example code') // eslint-disable-line no-console
+import styles from './index.css'
+
+var p = document.createElement('p')
+p.innerText = 'Hello guys, I\'m new!'
+p.className = styles.local
+document.body.appendChild(p)
--- tools/config/webpack.js
+++ tools/config/webpack.js
@@ -1,5 +1,10 @@
 import BUILD_PATH from './BUILD_PATH'
 import path from 'path'
+import cssnext from 'postcss-cssnext'
+import precss from 'precss'
+import ExtractTextWebpackPlugin from 'extract-text-webpack-plugin'
+
+const extractCss = new ExtractTextWebpackPlugin('index.css')
 
 export default {
 	entry: [
@@ -12,7 +17,14 @@ export default {
 	},
 	module: {
 		loaders: [
-			{ test: /\.js$/, loader: 'babel' }
+			{ test: /\.js$/, loader: 'babel' },
+			{ test: /\.css$/, loader: extractCss.extract('style', 'css-loader?modules&importLoaders=1&localIdentName=[path][name]--[local]--[hash:base64:5]!postcss-loader') }
 		]
+	},
+	plugins: [
+		extractCss
+	],
+	postcss: function() {
+		return [ cssnext(), precss() ]
 	}
 }
