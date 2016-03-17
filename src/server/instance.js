76efb8939ea6863fad9612d79f791f3f85614d9b
Mick van Gelderen <mickvangelderen@gmail.com>
2016-03-17 12:50:55 +0100

    Moar logging

--- src/server/instance.js
+++ src/server/instance.js
@@ -1,8 +1,10 @@
 import server from './'
 import PORT from './config/PORT'
 
+console.log('Starting server...') // eslint-disable-line no-console
+
 const instance = server.listen(PORT, () => {
-	console.log(instance.address()) // eslint-disable-line no-console
+	console.log('Server started', instance.address()) // eslint-disable-line no-console
 })
 
 export default instance
